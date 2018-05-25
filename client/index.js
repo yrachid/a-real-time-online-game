02752aaabb2b92b2b0d3f74c4361d1405a0fe66d
othman853 <anuar.yasser@gmail.com>
2018-05-25 18:20:09 -0300

    Figuring out how to control connections

--- client/index.js
+++ client/index.js
@@ -2,6 +2,12 @@ const io = require('socket.io-client')('http://localhost:3000/')
 
 io.on('connect', () => {
   console.log('Connect')
+  io.emit('check-in', {nick: 'monkey'})
+  io.emit('hall-scan')
+})
+
+io.on('hall-scan:response', (data) => {
+  console.log(data)
 })
 
 io.on('disconnect', () => {
@@ -9,4 +15,3 @@ io.on('disconnect', () => {
 })
 
 io.on('sup', () => console.log('Sup'))
-
--- server/index.js
+++ server/index.js
@@ -7,12 +7,33 @@ const ioServer = io(httpServer, {
   path: '/'
 })
 
+const hall = () => ({
+  guests: [],
+
+  checkIn(id, nick) {
+    this.guests.push({id, nick})
+  },
+
+  checkOut(id) {
+    this.guests = this.guests.filter(guest => guest.id !== id)
+  }
+})
+
+const serverHall = hall()
+
 ioServer.on('connection', (client) => {
-  console.log('New Connection')
-  client.on('hello', (data) => {
-    console.log(data)
+
+  client.on('check-in', (data) => {
+    serverHall.checkIn(client.id, data.nick)
+  })
+
+  client.on('hall-scan', () => {
+    client.emit('hall-scan:response', serverHall.guests)
+  })
+
+  client.on('disconnect', () => {
+    serverHall.checkOut(client.id)
   })
 })
 
 httpServer.listen(3000, () => console.log('UP'))
-
